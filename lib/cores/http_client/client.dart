beeea7ea6774a4824f613128431c71d377cdc92a
Fredrick Widjaya <f.widjaya20@gmail.com>
2020-08-25 10:50:31 +0700

    Adding Dependency for HttpClientAdapter (For Mocking)

--- lib/cores/http_client/client.dart
+++ lib/cores/http_client/client.dart
@@ -5,7 +5,8 @@ abstract class HttpClient {
   Dio _dio;
 
   HttpClient({
-    String baseUrl
+    String baseUrl,
+    HttpClientAdapter adapter
   }) {
     BaseOptions options = BaseOptions(
       baseUrl: baseUrl,
@@ -14,28 +15,32 @@ abstract class HttpClient {
 
     this._dio = Dio(options);
 
+    if (adapter != null) {
+      this._dio.httpClientAdapter = adapter;
+    }
+
     this.interceptors();
   }
 
   void interceptors() {
     this._dio.interceptors.add(InterceptorsWrapper(
-        onRequest: (RequestOptions options) {
-          debugPrint("[DIO DEBUG] Request : { baseUrl: ${options.baseUrl}, path: ${options.path}, method: ${options.method}, contentType: ${options.contentType}, headers: ${options.headers}, queryParams: ${options.queryParameters}, data: ${options.data} }");
+      onRequest: (RequestOptions options) {
+        debugPrint("[DIO DEBUG] Request : { baseUrl: ${options.baseUrl}, path: ${options.path}, method: ${options.method}, contentType: ${options.contentType}, headers: ${options.headers}, queryParams: ${options.queryParameters}, data: ${options.data} }");
 
-          return options;
-        },
-        onResponse: (Response response) {
-          debugPrint("[DIO DEBUG] Response : $response");
-          return response;
-        },
-        onError: (DioError error) async {
-          if(error.response?.data != null){
-            debugPrint("[DIO DEBUG] Error Response : ${error.response.data}");
-          }
+        return options;
+      },
+      onResponse: (Response response) {
+        debugPrint("[DIO DEBUG] Response : $response");
+        return response;
+      },
+      onError: (DioError error) async {
+        if(error.response?.data != null){
           debugPrint("[DIO DEBUG] Error Response : ${error.response.data}");
-
-          return error;
         }
+        debugPrint("[DIO DEBUG] Error Response : ${error.response.data}");
+
+        return error;
+      }
     ));
   }
 
